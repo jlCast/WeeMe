0adb2fca26fe8144df609b4f93628783a8b21b43
Jose Luis <joseluis.castellanos@gmail.com>
2021-10-19 13:57:41 +0200

    Splash screen v1 + strings

--- app/src/main/java/com/myapp/weeme/MainActivity.kt
+++ app/src/main/java/com/myapp/weeme/MainActivity.kt
@@ -2,10 +2,18 @@ package com.myapp.weeme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import com.myapp.weeme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //SystemClock.sleep(2000)//Establecemos un retardo para la carga de la activity
+        setTheme(R.style.Theme_WeeMe)//Establecemos el tema para que deje de usar el de la Splash Screen
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
